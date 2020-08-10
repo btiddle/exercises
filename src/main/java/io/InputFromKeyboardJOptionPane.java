ba3943f8bb0e4169050ab5af2eeae45c73d98154
Benjamin Tiddle <benjamin@intheflow.com>
2020-08-10 09:23:53 -0700

    Add 5 new exercises
    * collections/HashMapExample.java
    * collections/LinkedHashMapExample.java
    * collections/TreeMapExample.java
    * io/GetInputFromKeyboard.java
    * io/InputFromKeyboardJOptionPane.java

---
+++ src/main/java/io/InputFromKeyboardJOptionPane.java
@@ -0,0 +1,13 @@
+package io;
+
+import javax.swing.JOptionPane;
+
+public class InputFromKeyboardJOptionPane {
+
+    public static void main(String[] args) {
+        String name = "";
+        name=JOptionPane.showInputDialog("Please enter your name");
+        String msg = "Hello " + name + "!";
+        JOptionPane.showMessageDialog(null, msg);
+    }
+}
